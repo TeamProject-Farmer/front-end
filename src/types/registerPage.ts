22ff605cd701e93ea58418df9cf6a4415507ae52
hyeone999 <caps026@naver.com>
2023-06-11 11:28:25 +0900

    feat: Register Page type 생성

---
+++ src/types/registerPage.ts
@@ -0,0 +1,13 @@
+export interface IAuthForm {
+  email: string;
+  selectedEmail: string;
+  password: string;
+  passwordConfirm: string;
+  name: string;
+  phone: string;
+  postCode: string;
+  basicAddress: string;
+  detailAddress: string;
+  nickname: string;
+  checked: boolean;
+}
