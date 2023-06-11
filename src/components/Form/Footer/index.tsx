9650acf2f82a29e229eb5c5801fadad668fd0809
hyeone999 <caps026@naver.com>
2023-06-11 11:25:29 +0900

    design: Register Page Footer 생성

---
+++ src/components/Form/Footer/index.tsx
@@ -0,0 +1,15 @@
+import { Styled } from '@pages/register/styles';
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <Styled.LoginText>
+        이미 아이디가 있으신가요?
+        <span onClick={() => console.log('Login')}>로그인</span>
+      </Styled.LoginText>
+    </>
+  );
+};
+
+export default Footer;
