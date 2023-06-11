236754a26227584bc3eca1418520c3ea48d30585
hyeone999 <caps026@naver.com>
2023-06-11 11:28:48 +0900

    feat: common type 생성

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+// FormButton component props
+export interface ButtonProps {
+  label?: string;
+  color?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  onClick?: () => void;
+}
+
+// SnsCircle
+export interface CircleProps {
+  color?: string;
+  marginRight?: number;
+  marginLeft?: number;
+}
