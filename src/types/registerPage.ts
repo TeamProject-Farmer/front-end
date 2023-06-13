246783b609873cb5aa2a0066551be4e20eb1035c
hyeone999 <caps026@naver.com>
2023-06-13 09:18:16 +0900

    feat: input field types 생성

--- src/types/registerPage.ts
+++ src/types/registerPage.ts
@@ -1,3 +1,7 @@
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+// form types
 export interface IAuthForm {
   email: string;
   selectedEmail: string;
@@ -10,4 +14,14 @@ export interface IAuthForm {
   detailAddress: string;
   nickname: string;
   checked: boolean;
+}
+
+// input fields types
+export interface InputProps {
+  label: string;
+  inputProps: UseFormRegisterReturn;
+  error: string | null;
+  placeholder: string;
+  subText?: string | ReactJSXElement;
+  type?: string;
 }
