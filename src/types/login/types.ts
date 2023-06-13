4422222edfda79e841490fd52bba4d24e7b8705c
hyeone999 <caps026@naver.com>
2023-06-14 00:21:43 +0900

    feat: error text 종류 enum 추가

--- src/types/login/types.ts
+++ src/types/login/types.ts
@@ -1,3 +1,11 @@
+// error message
+export enum ErrorText {
+  EmailRequired = '이메일을 확인해주세요.',
+  PasswordRequired = '비밀번호를 확인해주세요.',
+  LoginFailed = '이메일 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.',
+}
+
+// styles
 export interface StyleProps {
   size: number;
   color: string;
