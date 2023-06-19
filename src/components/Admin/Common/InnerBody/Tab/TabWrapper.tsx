573b6eb36ce47c2ddd5e42291210aaa2c37aa376
blue7918 <kkiu978@gmail.com>
2023-06-19 13:39:09 +0900

    Feat: Add InnerBody > Tab

---
+++ src/components/Admin/Common/InnerBody/Tab/TabWrapper.tsx
@@ -0,0 +1,15 @@
+import Styled from './styles';
+import SelectAllButton from '../../SelectAllButton';
+
+const TabWrapper = props => {
+  return (
+    <>
+      <Styled.TabWrapper>
+        <Styled.TabContainer>{props.children}</Styled.TabContainer>
+        <SelectAllButton text="전체 선택"></SelectAllButton>
+      </Styled.TabWrapper>
+    </>
+  );
+};
+
+export default TabWrapper;
