909cd413c607f096cd985a09e27a1c5d10d62ad8
blue7918 <kkiu978@gmail.com>
2023-06-19 13:41:45 +0900

    Feat: InnerBody index

---
+++ src/components/Admin/Common/InnerBody/index.tsx
@@ -0,0 +1,23 @@
+import Styled from './styles';
+import TabWrapper from './Tab/TabWrapper';
+import FooterButtonWrapper from '../FooterButtonWrapper';
+interface PropsList {
+  tabProps: any;
+  innerBoxProps: any;
+  footerButtonProps: any;
+}
+
+const InnerBody = (props: PropsList) => {
+  const { tabProps, innerBoxProps, footerButtonProps } = props;
+  return (
+    <>
+      <Styled.Wrapper>
+        <TabWrapper>{tabProps}</TabWrapper>
+        <Styled.BodyWrapper>{innerBoxProps}</Styled.BodyWrapper>
+        <FooterButtonWrapper>{footerButtonProps}</FooterButtonWrapper>
+      </Styled.Wrapper>
+    </>
+  );
+};
+
+export default InnerBody;
