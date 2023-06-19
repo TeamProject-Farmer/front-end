b601abf806216eb32be47b2f2e08a7e8cec2ac40
blue7918 <kkiu978@gmail.com>
2023-06-19 15:23:48 +0900

    Fix: CurrentTime remove library

--- src/components/Admin/Common/CurrentTime.tsx
+++ src/components/Admin/Common/CurrentTime.tsx
@@ -1,36 +1,29 @@
 import { useState, useEffect } from 'react';
 import Styled from './styles';
-// import dynamic from 'next/dynamic';
-import Clock from 'react-live-clock';
-// import moment from 'moment';
-// import 'moment/locale/ko';
-// const Clock = dynamic(() => import('react-live-clock'), { ssr: false ,});
-//아직 어떤거 사용할지 고려 중
 
 const CurrentTime = () => {
-  // const [currentTime, setCurrentTime] = useState(null);
-  
-  // useEffect(() => {
-  //   const timeId = setInterval(() => handleTick(), 1000);
+  const [currentTime, setCurrentTime] = useState(null);
 
-  //   return () => {
-  //     clearInterval(timeId);
-  //   };
-  // });
-
-  // const handleTick = () => {
-  //   setCurrentTime(moment().format('YYYY년 MM월 DD일 HH:mm:ss'));
-  // };
+  useEffect(() => {
+    const timeId = setInterval(() => handleTick(), 1000);
+    return () => {
+      clearInterval(timeId);
+    };
+  });
+  const handleTick = () => {
+    setCurrentTime(new Date());
+  };
   return (
     <>
       <Styled.TimeWrapper>
-        <Clock
-          format={'YYYY년 MM월 DD일 HH:mm:ss'}
-          ticking={true}
-          timezone={'Asia/Seoul'}
-          noSsr={true}
-        />
-        {/* {currentTime} */}
+        {currentTime && (
+          <>
+            <span>{currentTime.getFullYear()}년 &nbsp;</span>
+            <span>{currentTime.getMonth() + 1}월 &nbsp;</span>
+            <span>{currentTime.getDate()}일 &nbsp;</span>
+            <span>{currentTime.toLocaleTimeString()}</span>
+          </>
+        )}
       </Styled.TimeWrapper>
     </>
   );
