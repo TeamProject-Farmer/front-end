e317b993fa67240bfdb2b5525d217e1c1e99e3cc
blue7918 <kkiu978@gmail.com>
2023-06-20 10:36:58 +0900

    Feat: CurrentTime Content

--- src/components/Admin/Common/CurrentTime.tsx
+++ src/components/Admin/Common/CurrentTime.tsx
@@ -1,29 +1,29 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Styled from './styles';
 
 const CurrentTime = () => {
-  // const [currentTime, setCurrentTime] = useState(null);
+  const [currentTime, setCurrentTime] = useState(null);
 
-  // useEffect(() => {
-  //   const timeId = setInterval(() => handleTick(), 1000);
-  //   return () => {
-  //     clearInterval(timeId);
-  //   };
-  // });
-  // const handleTick = () => {
-  //   setCurrentTime(new Date());
-  // };
+  useEffect(() => {
+    const timeId = setInterval(() => handleTick(), 1000);
+    return () => {
+      clearInterval(timeId);
+    };
+  });
+  const handleTick = () => {
+    setCurrentTime(new Date());
+  };
   return (
     <>
-      <Styled.TimeWrapper> 
-        {/* {currentTime && (
+      <Styled.TimeWrapper>
+        {currentTime && (
           <>
             <span>{currentTime.getFullYear()}년 &nbsp;</span>
             <span>{currentTime.getMonth() + 1}월 &nbsp;</span>
             <span>{currentTime.getDate()}일 &nbsp;</span>
             <span>{currentTime.toLocaleTimeString()}</span>
           </>
-        )} */}time
+        )}
       </Styled.TimeWrapper>
     </>
   );
