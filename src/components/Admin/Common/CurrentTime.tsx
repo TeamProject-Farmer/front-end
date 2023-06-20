ab0652748f1adc466650e314a12723dda5abb2bf
blue7918 <kkiu978@gmail.com>
2023-06-20 10:40:51 +0900

    Feat: react-live-clock

--- src/components/Admin/Common/CurrentTime.tsx
+++ src/components/Admin/Common/CurrentTime.tsx
@@ -1,29 +1,36 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import Styled from './styles';
+import Clock from 'react-live-clock';
 
 const CurrentTime = () => {
-  const [currentTime, setCurrentTime] = useState(null);
+  // const [currentTime, setCurrentTime] = useState(null);
 
-  useEffect(() => {
-    const timeId = setInterval(() => handleTick(), 1000);
-    return () => {
-      clearInterval(timeId);
-    };
-  });
-  const handleTick = () => {
-    setCurrentTime(new Date());
-  };
+  // useEffect(() => {
+  //   const timeId = setInterval(() => handleTick(), 1000);
+  //   return () => {
+  //     clearInterval(timeId);
+  //   };
+  // });
+  // const handleTick = () => {
+  //   setCurrentTime(new Date());
+  // };
   return (
     <>
       <Styled.TimeWrapper>
-        {currentTime && (
+        {/* {currentTime && (
           <>
             <span>{currentTime.getFullYear()}년 &nbsp;</span>
             <span>{currentTime.getMonth() + 1}월 &nbsp;</span>
             <span>{currentTime.getDate()}일 &nbsp;</span>
             <span>{currentTime.toLocaleTimeString()}</span>
           </>
-        )}
+        )} */}
+        <Clock
+          format={'YYYY년 MM월 DD일 HH:mm:ss'}
+          ticking={true}
+          timezone={'Asia/Seoul'}
+          noSsr={true}
+        />
       </Styled.TimeWrapper>
     </>
   );
