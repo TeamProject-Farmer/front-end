a807a7c58795aa29097a91a7dd93f107edf32307
blue7918 <kkiu978@gmail.com>
2023-06-23 14:50:47 +0900

    Feat: Modify Sidebar layout

--- src/components/Admin/AdminBody/Account/AccountBody.tsx
+++ src/components/Admin/AdminBody/Account/AccountBody.tsx
@@ -1,39 +1,95 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import InnerBody from '@components/Admin/Common/InnerBody';
 import AccountInnerBox from './AccountInnerBox';
 import SingleTab from '../../Common/InnerBody/Tab/SingleTab';
 import SmallButton from '../../Common/FooterButtonWrapper/SmallButton';
 import ManageAccount from '@components/Admin/Common/Modal/ManageAccount';
+import { accountList, accountSearch } from 'src/apis/admin/account';
 
 const AccountBody = () => {
-  const [modalOpen, setModalOpen] = useState(0);
-
-  function openModal() {
+  const [modalOpen, setModalOpen] = useState<number>(0);
+  const [fieldName, setFieldName] = useState<string>('username');
+  const [pageNum, setPageNumber] = useState<number>(0);
+  const [data, setData] = useState<string>('');
+  const openModal = () => {
     setModalOpen(2);
-  }
-  function closeModal() {
+  };
+  const closeModal = () => {
     setModalOpen(0);
-  }
+  };
+  let innerBoxList: string = null;
+  const handleAccountList = async () => {
+    try {
+      const res = await accountList(fieldName);
+      console.log(res.data.content);
+      console.log(res.data.content[0]);
+      console.log(res.data.totalElements);
+      const Info = res.data.content.map(i => {
+        return {
+          nickname: i.nickname,
+          manager: i.grade,
+          registerDate: i.id,
+          role: i.role,
+        };
+      });
+      setData(Info);
+      for (let t = 0; t < res.data.totalElements; t++) {
+        
+      }
+      
+      // innerBoxList =
+      //   res.data.content &&
+      //   res.data.content.map(i => {
+      //     return (
+      //       <AccountInnerBox
+      //         nickname={i.nickname}
+      //         manager={i.grade}
+      //         registerDate="2023-04-02"
+      //         role={i.role}
+      //       ></AccountInnerBox>
+      //     );
+      //   });
+      innerBoxList = data;
+      console.log(innerBoxList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log('data');
+  console.log(data);
+  const handleAccountSearch = async () => {
+    try {
+      const response = await accountSearch(15);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    //test code
+    handleAccountList();
+    handleAccountSearch();
+  }, []);
+
   return (
     <>
       {modalOpen === 2 ? (
         <ManageAccount id={1} modalClose={closeModal} />
       ) : null}
+      {innerBoxList}
       <InnerBody
         tabProps={
           <>
-            <SingleTab text="등록일순" />
-            <SingleTab text="이름순" />
+            <SingleTab
+              name="createdDate"
+              text="등록일순"
+              fieldName={fieldName}
+              setState={setFieldName}
+            />
+            <SingleTab name="username" text="이름순" fieldName={fieldName} />
           </>
         }
-        innerBoxProps={
-          <AccountInnerBox
-            nickname="Sarah"
-            manager="슈퍼관리자"
-            registerDate="2023-04-02"
-            role="쇼핑몰 관리자"
-          ></AccountInnerBox>
-        }
+        innerBoxProps={innerBoxList}
         footerButtonProps={
           <>
             <SmallButton text="추가" modalOpen={openModal} />
