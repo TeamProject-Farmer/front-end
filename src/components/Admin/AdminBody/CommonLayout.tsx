f0925f071f8f2983f5bcaceed766d23501fbe8d5
blue7918 <kkiu978@gmail.com>
2023-06-23 15:05:24 +0900

    Fix: if to switch

--- src/components/Admin/AdminBody/CommonLayout.tsx
+++ src/components/Admin/AdminBody/CommonLayout.tsx
@@ -5,7 +5,7 @@ import AccountBody from './Account/AccountBody';
 import MemberBody from './Member/MemberBody';
 import CartBody from './Cart/CartBody';
 import BoardBody from './Board/BoardBody';
-import ProductrBody from './Product/ProductBody';
+import ProductBody from './Product/ProductBody';
 
 interface PropsList {
   cate: string | string[]; //<-- 왜 string[]인지 모르겠음
@@ -15,23 +15,30 @@ const CommonLayout = (props: PropsList) => {
   let pageComponent = null;
   let headerTitle: string;
 
-  if (cate == 'account') {
-    headerTitle = '계정관리';
-    pageComponent = <AccountBody />;
-  } else if (cate === 'member') {
-    headerTitle = '회원관리';
-    pageComponent = <MemberBody />;
-  } else if (cate == 'cart') {
-    headerTitle = '주문관리';
-    pageComponent = <CartBody />;
-  } else if (cate == 'product') {
-    headerTitle = '상품관리';
-    pageComponent = <ProductrBody />;
-  } else if (cate == 'board') {
-    headerTitle = '게시판관리';
-    pageComponent = <BoardBody />;
-  } else {
-    //예외처리하기
+  switch (cate) {
+    case 'account':
+      headerTitle = '계정관리';
+      pageComponent = <AccountBody />;
+      break;
+    case 'member':
+      headerTitle = '회원관리';
+      pageComponent = <MemberBody />;
+      break;
+    case 'cart':
+      headerTitle = '주문관리';
+      pageComponent = <CartBody />;
+      break;
+    case 'product':
+      headerTitle = '상품관리';
+      pageComponent = <ProductBody />;
+      break;
+    case 'board':
+      headerTitle = '게시판관리';
+      pageComponent = <BoardBody />;
+      break;
+    default:
+      // 예외 처리하기
+      break;
   }
   return (
     <AdminLayout>
