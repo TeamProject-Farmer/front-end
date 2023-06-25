be021fce53bbfc6cf8d862b6a7b6f4138e5db7d4
hyeone999 <caps026@naver.com>
2023-06-25 18:43:54 +0900

    feat: kakao 로그인 버튼 path 설정

--- src/components/Register/SnsCircles/index.tsx
+++ src/components/Register/SnsCircles/index.tsx
@@ -2,13 +2,20 @@ import Styled from '../styles';
 import Google from '@assets/images/login/google.svg';
 import Kakao from '@assets/images/login/kakao.svg';
 import Naver from '@assets/images/login/naver.svg';
+import Link from 'next/link';
+import { KAKAO_AUTH_URL } from 'src/utils/register/socialLoginData';
 
 // 백엔드 소셜로그인 기능 구현되면 API 적용 예정
 const SnsCircles = () => {
   return (
     <Styled.IconWrapper>
       <Google className="cursor-pointer" />
-      <Kakao className="cursor-pointer" />
+
+      {/* 카카오 로그인 */}
+      <Link href={KAKAO_AUTH_URL}>
+        <Kakao className="cursor-pointer" />
+      </Link>
+
       <Naver className="cursor-pointer" />
     </Styled.IconWrapper>
   );
