59c228ec2c171da1d2fadc8d67b34ae24ab47020
blue7918 <kkiu978@gmail.com>
2023-06-25 23:01:08 +0900

    Feat: Modify Board star part

--- src/components/Admin/AdminBody/Board/BoardInnerBox.tsx
+++ src/components/Admin/AdminBody/Board/BoardInnerBox.tsx
@@ -1,71 +1,20 @@
 import Styled from './styles';
 import CheckBox from '@components/Admin/Common/InnerBody/InnerBox/CheckBox';
-import {boardInnerPropsList} from '@components/Admin/Type'
+import { boardInnerPropsList } from '@components/Admin/Type';
 
 const BoardInnerBox = (props: boardInnerPropsList) => {
   const { id, title, like, buyer, commentTitle, comment } = props;
-  let stars = null;
+
   const filledStar = <Styled.Star />;
   const blankStar = <Styled.BlankStar />;
-  // const maxStars = 5;
-  // let stars = (
-  //   <Styled.StarWrapper>
-  //     {Array.from({ length: maxStars }, (_, index) =>
-  //       index < like ? filledStar : blankStar,
-  //     )}
-  //   </Styled.StarWrapper>
-  // );
-  if (like == 1) {
-    stars = (
-      <Styled.StarWrapper>
-        {filledStar}
-        {blankStar}
-        {blankStar}
-        {blankStar}
-        {blankStar}
-      </Styled.StarWrapper>
-    );
-  } else if (like == 2) {
-    stars = (
-      <Styled.StarWrapper>
-        {filledStar}
-        {filledStar}
-        {blankStar}
-        {blankStar}
-        {blankStar}
-      </Styled.StarWrapper>
-    );
-  } else if (like == 3) {
-    stars = (
-      <Styled.StarWrapper>
-        {filledStar}
-        {filledStar}
-        {filledStar}
-        {blankStar}
-        {blankStar}
-      </Styled.StarWrapper>
-    );
-  } else if (like == 4) {
-    stars = (
-      <Styled.StarWrapper>
-        {filledStar}
-        {filledStar}
-        {filledStar}
-        {filledStar}
-        {blankStar}
-      </Styled.StarWrapper>
-    );
-  } else {
-    stars = (
-      <Styled.StarWrapper>
-        {filledStar}
-        {filledStar}
-        {filledStar}
-        {filledStar}
-        {filledStar}
-      </Styled.StarWrapper>
-    );
-  }
+  const maxStars = 5;
+  let stars = (
+    <Styled.StarWrapper>
+      {Array.from({ length: maxStars }, (_, index) =>
+        index < like ? filledStar : blankStar,
+      )}
+    </Styled.StarWrapper>
+  );
 
   return (
     <>
