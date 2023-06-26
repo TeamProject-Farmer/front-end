ad65df0b7613959db156bb60b7e99f11718d5fc8
hyeone999 <caps026@naver.com>
2023-06-26 12:47:23 +0900

    rename: path 변경 적용

--- src/components/Register/SnsCircles/index.tsx
+++ src/components/Register/SnsCircles/index.tsx
@@ -3,7 +3,7 @@ import Google from '@assets/images/login/google.svg';
 import Kakao from '@assets/images/login/kakao.svg';
 import Naver from '@assets/images/login/naver.svg';
 import Link from 'next/link';
-import { KAKAO_AUTH_URL } from 'src/utils/register/socialLoginData';
+import { KAKAO_AUTH_URL } from 'src/utils/login/socialLoginData';
 
 // 백엔드 소셜로그인 기능 구현되면 API 적용 예정
 const SnsCircles = () => {
