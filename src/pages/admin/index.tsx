eb4429800d039f346a02c218c9761c9d525f49b1
blue7918 <kkiu978@gmail.com>
2023-07-01 23:32:11 +0900

    Feat: pages > admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,30 @@
-const AdminPage = () => {
-  return <></>;
-}
+import { useRouter } from 'next/router';
+import styled from '@emotion/styled';
+import SideBar from '@components/Admin/SideBar';
+import AdminLayout from '@components/Admin/AdminLayout';
 
-export default AdminPage;
+const admin = () => {
+  const router = useRouter();
+  const menu = router.query.menu;
+  let category: string;
+  if (menu) {
+    category = menu.toString();
+  }
+  return (
+    <Wrapper>
+      <SideBar menuName={category} />
+      <AdminLayout page='Home' />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  max-width: 100%;
+  height: 100vh;
+  max-height: 100%;
+  background-color: #f7f7f7;
+`;
+
+export default admin;
