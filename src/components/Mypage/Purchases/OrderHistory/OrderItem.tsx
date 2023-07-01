69aa279d7700c287161b7f30751e37d4b1548f31
hyeone999 <caps026@naver.com>
2023-07-01 17:29:16 +0900

    feat: 구매목록 페이지 구현

--- src/components/Mypage/Purchases/OrderHistory/OrderItem.tsx
+++ src/components/Mypage/Purchases/OrderHistory/OrderItem.tsx
@@ -1,35 +1,36 @@
-import { Styled } from '@components/Mypage/styles';
 import theme from '@styles/theme';
 import React from 'react';
 import { OrderProps } from 'src/types/mypage/types';
+import { Styled } from '@components/Mypage/styles';
+import OrderInfoText from './OrderInfoText';
 
 export const OrderItem = ({ order }: { order: OrderProps }) => {
   return (
     <Styled.FlexRowCenter>
-      <Styled.ListInfoText size="20" color={theme.colors.black}>
-        {order.date}
-      </Styled.ListInfoText>
-      {/* 임시 이미지 박스 */}
+      <OrderInfoText size="20" color={theme.colors.black} text={order.date} />
+      {/* 임시 이미지 박스 - 이미지 클릭시 해당 제품 상세 페이지 */}
       <Styled.Image />
       <Styled.ProductInfoWrapper>
-        <Styled.ListInfoText size="16" color={theme.colors.black}>
-          {order.productName}
-        </Styled.ListInfoText>
-        <Styled.ListInfoText size="16" color={theme.colors.mypageGray}>
-          {order.productDescription}
-        </Styled.ListInfoText>
-        {/* 상세 클릭시 어떤 기능인지 미정 */}
+        <OrderInfoText
+          size="16"
+          color={theme.colors.black}
+          text={order.productName}
+        />
+        <OrderInfoText
+          size="16"
+          color={theme.colors.mypageGray}
+          text={order.productDescription}
+        />
+        {/* 상세 클릭시 주문 상세보기 이동 */}
         <Styled.ProductButton>제품상세확인</Styled.ProductButton>
       </Styled.ProductInfoWrapper>
-      <Styled.ListInfoText size="20" color={theme.colors.black}>
-        {order.quantity}
-      </Styled.ListInfoText>
-      <Styled.ListInfoText size="20" color={theme.colors.black}>
-        {order.price}
-      </Styled.ListInfoText>
-      <Styled.ListInfoText size="20" color={theme.colors.black}>
-        {order.status}
-      </Styled.ListInfoText>
+      <OrderInfoText
+        size="20"
+        color={theme.colors.black}
+        text={order.quantity}
+      />
+      <OrderInfoText size="20" color={theme.colors.black} text={order.price} />
+      <OrderInfoText size="20" color={theme.colors.black} text={order.status} />
     </Styled.FlexRowCenter>
   );
 };
