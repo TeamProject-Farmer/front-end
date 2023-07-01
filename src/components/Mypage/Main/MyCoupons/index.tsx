b912aedae841438d3b9d6d32ec016a4ef43e96af
hyeone999 <caps026@naver.com>
2023-07-01 11:46:10 +0900

    feat: 쿠폰 페이지 생성

---
+++ src/components/Mypage/Main/MyCoupons/index.tsx
@@ -0,0 +1,27 @@
+import { Styled } from '@components/Mypage/styles';
+import React from 'react';
+import { couponsData } from 'src/utils/mypage/coupons';
+
+// 맥북 기준 3개까지 정렬가능 (반응형 필요)`
+
+const Row = () => {
+  return (
+    <Styled.BoxWrapper>
+      <Styled.CardBox>
+        <Styled.HeaderDiv>
+          <Styled.HeaderText>나의 쿠폰</Styled.HeaderText>
+        </Styled.HeaderDiv>
+        <Styled.CouponWrapper>
+          {couponsData.map((coupon, index) => (
+            <Styled.Coupon key={index}>
+              {coupon.icon}
+              <h1>{coupon.title}</h1>
+            </Styled.Coupon>
+          ))}
+        </Styled.CouponWrapper>
+      </Styled.CardBox>
+    </Styled.BoxWrapper>
+  );
+};
+
+export default Row;
