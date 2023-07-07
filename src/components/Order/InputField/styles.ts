1780f4419f72acc68f5bbdbbf7a1655d283b10e2
zwonkim <k980912@naver.com>
2023-07-08 00:32:18 +0900

    style: select style 변경

--- src/components/Order/InputField/styles.ts
+++ src/components/Order/InputField/styles.ts
@@ -24,12 +24,6 @@ const Styled = {
     padding-left: 10px;
     border: 1px solid ${theme.colors.borderGray};
   `,
-  IconWrapper: styled.div`
-    position: absolute;
-    top: 14px;
-    right: 13px;
-    cursor: pointer;
-  `,
   Dropdown: styled.select<{ isMobile?: boolean }>`
     width: ${({ isMobile }) => (isMobile ? '130px' : '750px')};
     height: 40px;
@@ -37,6 +31,18 @@ const Styled = {
     border-radius: 4px;
     border: 1px solid #ababab;
     background-color: #fff;
+    -o-appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    ::-ms-expand {
+      display: none;
+    }
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='11' height='6' fill='none'%3E%3Cpath fill='%23ABABAB' d='M5.5 6 .737.75h9.526L5.5 6Z'/%3E%3C/svg%3E");
+    background-repeat: no-repeat;
+    background-size: 15px 13px;
+    background-position: ${({ isMobile }) => (isMobile ? '100px' : '720px')}
+      center;
   `,
   Option: styled.option`
     font-size: 14px;
