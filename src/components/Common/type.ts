d61dadfb0105cf645ff116b657ab981d6e8a7375
blue7918 <kkiu978@gmail.com>
2023-07-24 22:19:06 +0900

    feat: product type

--- src/components/Common/type.ts
+++ src/components/Common/type.ts
@@ -1,9 +1,8 @@
 export interface ProductProps {
-  //number | string 이 부분은 나중에 좀 더 얘기해봐야할 것 같습니다!
   thumbnailImg: string;
   name: string;
-  price: number | string;
+  price: Number;
   discountRate: number;
-  reviewCount: number | string;
+  reviewCount: number;
   averageStarRating: number;
 }
