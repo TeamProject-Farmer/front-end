e91db1c00e62d0c6c5233feb27ecb486e061448f
hyeone999 <caps026@naver.com>
2023-08-07 19:49:00 +0900

    feat: 장바구니 페이지 all check 구현

--- src/components/Mypage/Cart/CartList.tsx
+++ src/components/Mypage/Cart/CartList.tsx
@@ -3,14 +3,28 @@ import { Styled } from '../styles';
 import { cartLabels } from 'src/utils/mypage/orderTimeList';
 import theme from '@styles/theme';
 import CartItems from './CartItems';
+import { CartListProps } from 'src/types/mypage/types';
+import { RootState } from 'store';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { toggleAllChecked } from 'store/reducers/cartIndexSlice';
+const cartSelector = (state: RootState) => state.cartIndex;
 
-const CartList = () => {
+const CartList = ({ cartListArray }: { cartListArray: CartListProps[] }) => {
+  const dispatch = useDispatch();
+  const selector = useSelector(cartSelector);
+
   return (
     <Styled.CheckWrapper>
       <Styled.ListText>장바구니 목록</Styled.ListText>
 
+      {/* 리스트 상단 */}
       <Styled.FlexCartRow>
-        <input type="checkbox" style={{ width: '16px', height: '16px' }} />
+        <Styled.CartCheckBox
+          checked={selector.allChecked || false}
+          onChange={() => dispatch(toggleAllChecked())}
+          type="checkbox"
+        />
         {cartLabels.map((text, index) => (
           <Styled.ListInfoText key={index} size="16" color={theme.colors.white}>
             {text}
@@ -18,10 +32,10 @@ const CartList = () => {
         ))}
       </Styled.FlexCartRow>
 
-      <CartItems />
+      <CartItems cartListArray={cartListArray} />
 
-      {/* 클릭하면 제품 페이지로 */}
       <Styled.CartCommonButton
+        onClick={() => console.log('쇼핑 페이지로')}
         backgroundColor={theme.colors.cartButtonGray}
         padding="0.7"
         width="125"
