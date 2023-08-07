06f2893891068d74887e5fa5077833917d0d6761
hyeone999 <caps026@naver.com>
2023-08-07 19:48:25 +0900

    feat: cart 페이지 데이터값 props 내려주기

--- src/pages/mypage/cart.tsx
+++ src/pages/mypage/cart.tsx
@@ -1,14 +1,22 @@
+import React from 'react';
 import CartList from '@components/Mypage/Cart/CartList';
 import OrderCartList from '@components/Mypage/Cart/OrderCartList';
 import Layout from '@components/Mypage/Layout/layout';
-import React from 'react';
+import { useQuery } from 'react-query';
+import { getCartList } from 'src/apis/mypage/cart';
+import { CartListProps } from 'src/types/mypage/types';
 
 const index = () => {
+  const { data: cartListArray } = useQuery<CartListProps[]>(
+    'cartList',
+    getCartList,
+  );
+
   return (
     <Layout>
-      <CartList />
+      <CartList cartListArray={cartListArray} />
 
-      <OrderCartList />
+      <OrderCartList cartListArray={cartListArray} />
     </Layout>
   );
 };
