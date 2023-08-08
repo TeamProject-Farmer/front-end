61e72540266f3d33b571241fc445f2774db17eb2
hyeone999 <caps026@naver.com>
2023-08-08 12:52:12 +0900

    feat: 장바구니 선택 기능, 옵션 기능 구현

--- src/components/Mypage/Cart/CartItems.tsx
+++ src/components/Mypage/Cart/CartItems.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Styled } from '../styles';
 import theme from '@styles/theme';
 import { CartListProps } from 'src/types/mypage/types';
 import Image from 'next/image';
 import { getEditCount } from 'src/apis/mypage/cart';
 import { useQueryClient } from 'react-query';
+import { useDispatch } from 'react-redux';
+import { setCartIndex, setChecked } from 'store/reducers/cartSlice';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store';
+const cartSelector = (state: RootState) => state.cartIndex;
 
 const CartItems = ({ cartListArray }: { cartListArray: CartListProps[] }) => {
   const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+  const selector = useSelector(cartSelector);
+  const lengthChecked = cartListArray?.length === selector.idArray.length;
 
   // 상품 개수 변경 기능
   const handleEditCount = async (id: number, operation: 'minus' | 'plus') => {
@@ -15,9 +23,39 @@ const CartItems = ({ cartListArray }: { cartListArray: CartListProps[] }) => {
       await getEditCount(id, operation);
       await queryClient.invalidateQueries('cartList');
     } catch (error) {
-      console.error('에러 발생:', error);
+      console.error(error);
     }
   };
+
+  // 상품 선택 기능
+  const handleCheckboxClick = (cartId: number) => {
+    dispatch(
+      setCartIndex({
+        idArray: selector.idArray.includes(cartId)
+          ? selector.idArray.filter(id => id !== cartId)
+          : [...selector.idArray, cartId],
+      }),
+    );
+  };
+
+  // 전체선택 및 전체해제 싸이클 관리
+  useEffect(() => {
+    if (cartListArray) {
+      if (selector.allChecked) {
+        const allChecked = cartListArray.map(item => item.cartId);
+        dispatch(setCartIndex({ idArray: allChecked }));
+      } else if (lengthChecked && !selector.allChecked) {
+        dispatch(setCartIndex({ idArray: [] }));
+      }
+    }
+  }, [selector.allChecked]);
+
+  // 개별적으로 모든 아이템 선택시 -> 전체선택 || 전체선택에서 하나라도 해제시 -> 전체선택 해제
+  useEffect(() => {
+    if (cartListArray) {
+      lengthChecked ? dispatch(setChecked(true)) : dispatch(setChecked(false));
+    }
+  }, [selector.idArray]);
 
   return (
     <>
@@ -28,9 +66,21 @@ const CartItems = ({ cartListArray }: { cartListArray: CartListProps[] }) => {
             index === cartListArray.length - 1 ? { marginBottom: '2rem' } : null
           }
         >
-          <Styled.CartCheckBox type="checkbox" />
+          <Styled.CartCheckBox
+            checked={selector.idArray.includes(item.cartId)}
+            onChange={() => handleCheckboxClick(item.cartId)}
+            type="checkbox"
+          />
 
-          <Image src={item.imgUrl} alt="식물 사진" width="100" height="100" />
+          <Image
+            src={item.imgUrl}
+            priority
+            alt="식물 사진"
+            width={100}
+            height={100}
+            // aspect ratio error fix
+            style={{ width: 'auto', height: 'auto' }}
+          />
 
           <Styled.ListInfoText size="18" color={theme.colors.black}>
             {item.productName} <br />
@@ -40,13 +90,13 @@ const CartItems = ({ cartListArray }: { cartListArray: CartListProps[] }) => {
             <Styled.QuantityButton
               // 상품 개수가 1개면 마이너스 되지 않도록
               disabled={item.count === 1}
-              onClick={() => handleEditCount(item.optionId, 'minus')}
+              onClick={() => handleEditCount(item.cartId, 'minus')}
             >
               -
             </Styled.QuantityButton>
             <Styled.QuantityText>{item.count}</Styled.QuantityText>
             <Styled.QuantityButton
-              onClick={() => handleEditCount(item.optionId, 'plus')}
+              onClick={() => handleEditCount(item.cartId, 'plus')}
             >
               +
             </Styled.QuantityButton>
