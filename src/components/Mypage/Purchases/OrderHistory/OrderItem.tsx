b5fddf6e1ad0d4cc642fd170ca8c56ce5484e950
hyeone999 <caps026@naver.com>
2023-08-09 15:47:37 +0900

    feat: 주문 목록페이지 -> 제품 상세 페이지 이동 구현(예정)

--- src/components/Mypage/Purchases/OrderHistory/OrderItem.tsx
+++ src/components/Mypage/Purchases/OrderHistory/OrderItem.tsx
@@ -8,7 +8,6 @@ export const OrderItem = ({ order }: { order: OrderProps }) => {
   return (
     <Styled.FlexRowCenter>
       <OrderInfoText size="20" color={theme.colors.black} text={order.date} />
-      {/* 임시 이미지 박스 - 이미지 클릭시 해당 제품 상세 페이지 */}
       <Styled.Image />
       <Styled.ProductInfoWrapper>
         <OrderInfoText
@@ -21,8 +20,8 @@ export const OrderItem = ({ order }: { order: OrderProps }) => {
           color={theme.colors.mypageGray}
           text={order.productDescription}
         />
-        {/* 상세 클릭시 주문 상세보기 이동 */}
-        <Styled.ProductButton>주문 상세보기</Styled.ProductButton>
+        {/* 상세보기 페이지로 이동 */}
+        <Styled.ProductButton>상품 상세보기</Styled.ProductButton>
       </Styled.ProductInfoWrapper>
       <OrderInfoText
         size="20"
