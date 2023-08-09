c3c6b2d6c0636c35df8a6dcb21f1772b54f70c19
hyeone999 <caps026@naver.com>
2023-08-09 17:33:14 +0900

    feat: 주문 처리상태 data 구현

---
+++ src/utils/mypage/orderStatusData.ts
@@ -0,0 +1,10 @@
+export const OptionList = [
+  { label: '전체 주문처리 상태', value: 'ALL' },
+  { label: '결제대기', value: 'WAIT' },
+  { label: '주문실패', value: 'FAIL' },
+  { label: '주문완료', value: 'DONE' },
+  { label: '주문취소', value: 'CANCEL' },
+  { label: '환불', value: 'REFUND' },
+  { label: '교환', value: 'EXCHANGE' },
+  { label: '반품', value: 'RETURN' },
+];
