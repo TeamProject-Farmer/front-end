c1f2a946ce69278a7c58614fce3c986691eafde2
blue7918 <kkiu978@gmail.com>
2023-08-10 22:14:27 +0900

    feat: photo review slide

--- src/components/Shop/DetailPage/PreviewPhoto.tsx
+++ src/components/Shop/DetailPage/PreviewPhoto.tsx
@@ -10,43 +10,48 @@ import { RootState } from 'store';
 export const idSelector = (state: RootState) => state.productId;
 
 const PreviewPhoto = () => {
-  const [imageList, setImageList] = useState([]);
   const productId = useSelector(idSelector);
+  const [imageList, setImageList] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const handleReviewImage = async () => {
     const response = await getReviewImage(productId);
     setImageList(response);
   };
+  let filtered: string[] = imageList.filter((value: string) => value != null);
+
+  const handleArrow = (move: number) => {
+    let nextIndex = currentIndex + move;
+    if (nextIndex <= 0 && nextIndex > - Math.ceil(filtered.length/5)) setCurrentIndex(nextIndex);
+  };
   useEffect(() => {
     handleReviewImage();
   }, []);
-  let filtered: string[] = imageList.filter((value: string) => value != null);
-  console.log('---------imageList------');
-  console.log(imageList);
-  console.log(filtered);
   return (
     <Styled.Wrapper>
-      <div>사진 리뷰 보기</div>
+      <Styled.Title>사진 리뷰 보기</Styled.Title>
       <Styled.ImageWrapper>
-        <Styled.LeftArrow />
+        <Styled.LeftArrow onClick={() => handleArrow( 1)} />
         <Styled.ImageBox>
-          {filtered.map((item, index) =>
-            item == null ? (
-              <></>
-            ) : (
-              <Styled.prevImageBox>
-                <Image
-                  key={index}
-                  alt="Preview"
-                  src={item}
-                  className="prevImage"
-                  width={229}
-                  height={229}
-                />
-              </Styled.prevImageBox>
-            ),
-          )}
+          <Styled.ImageSlider currentIndex={currentIndex}>
+            {filtered.map((item, index) =>
+              item == null ? (
+                <></>
+              ) : (
+                <Styled.prevImageBox>
+                  <Image
+                    key={index}
+                    alt="Preview"
+                    src={item}
+                    className="prevImage"
+                    width={229}
+                    height={229}
+                  />
+                </Styled.prevImageBox>
+              ),
+            )}
+          </Styled.ImageSlider>
         </Styled.ImageBox>
-        <Styled.RightArrow />
+        <Styled.RightArrow onClick={() => handleArrow(-1)} />
       </Styled.ImageWrapper>
     </Styled.Wrapper>
   );
@@ -61,40 +66,59 @@ const Styled = {
     width: ${theme.size.mainWidth};
     height: 367px;
     margin: 0 auto;
-    justify-content: center;
+  `,
+  Title: styled.div`
+    height: fit-content;
+    margin-top: 40px;
+    font-weight: 600;
+    font-size: 25px;
   `,
   ImageWrapper: styled.div`
-    margin-top: 16.3px;
+    height: 240px;
+    margin-top: 10px;
     position: relative;
     display: flex;
     align-items: center;
-    margin-left: -122px;
-    margin-right: -125px;
   `,
   ImageBox: styled.div`
     width: ${theme.size.mainWidth};
+    height: 240px;
+    position: relative;
+    overflow: hidden;
+  `,
+  ImageSlider: styled.div<{ currentIndex: number }>`
+    height: inherit;
+    width: fit-content;
     display: flex;
-    justify-content: space-between;
+    gap: 13px;
+    position: absolute;
+    top: 0;
+    left: ${props => props.currentIndex * 1200}px;
+    transition: 500ms;
   `,
   prevImageBox: styled.div`
     display: flex;
-      align-items: center;
-      justify-content: center;
+    align-items: center;
+    justify-content: center;
+    width: 229px;
+    height: 229px;
+    overflow: hidden;
+    border-radius: 10px;
+    .prevImage {
       width: 229px;
-      height: 229px;
-      overflow: hidden;
-      border-radius: 10px;
-      .prevImage {
-        width: 229px;
-        height: 240px;
-        object-fit: cover;
-      }
+      height: 240px;
+      object-fit: cover;
+    }
   `,
   LeftArrow: styled(leftArrow)`
-    margin-right: 53px;
+    position: absolute;
+    top: 60px;
+    left: -130px;
   `,
   RightArrow: styled(rightArrow)`
-    margin-left: 56px;
+    position: absolute;
+    top: 60px;
+    right: -130px;
   `,
 };
 
