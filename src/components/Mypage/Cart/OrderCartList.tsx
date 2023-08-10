36b54665ae39807490683d4bf524fcf2ed6fb49f
hyeone999 <caps026@naver.com>
2023-08-10 23:39:23 +0900

    feat: 장바구니 -> 주문 구현(라우팅은 병합 후 구현)

--- src/components/Mypage/Cart/OrderCartList.tsx
+++ src/components/Mypage/Cart/OrderCartList.tsx
@@ -4,16 +4,73 @@ import theme from '@styles/theme';
 import CartButtonComponent from './CartButtonComponent';
 import Plus from '@assets/images/mypage/cartPlus.svg';
 import Equals from '@assets/images/mypage/cartEquals.svg';
+import { CartListProps } from 'src/types/mypage/types';
+import { useSelector } from 'react-redux';
+import { clearCartIndex, setChecked } from 'store/reducers/cartSlice';
+import { useQueryClient } from 'react-query';
+import { useDispatch } from 'react-redux';
+import { RootState } from 'store';
+import { getRemoveCartList } from 'src/apis/mypage/cart';
+import { setOrderProduct } from 'store/reducers/orderProductSlice';
+const cartSelector = (state: RootState) => state.cartIndex;
 
-const OrderCartList = () => {
+const OrderCartList = ({
+  cartListArray,
+}: {
+  cartListArray: CartListProps[];
+}) => {
+  const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+  const selector = useSelector(cartSelector);
+
+  // CartListArray의 모든 객체의 total price 값을 합산
+  const handleTotalPrice = () => {
+    if (cartListArray) {
+      const totalPrice = cartListArray.reduce(
+        (acc, item) => acc + item.totalPrice,
+        0,
+      );
+      return totalPrice.toLocaleString();
+    }
+  };
+
+  // 장바구니 삭제 기능
+  const handleRemoveCartList = async () => {
+    await getRemoveCartList(selector.idArray);
+    await queryClient.invalidateQueries('cartList');
+    dispatch(clearCartIndex());
+    dispatch(setChecked(false));
+  };
+
+  // 장버구니에서 선택된 상품들을 저장
+  const getSelectedCartItems = () => {
+    if (!selector.idArray) {
+      return [];
+    }
+
+    return cartListArray?.filter(item =>
+      selector.idArray.includes(item.cartId),
+    );
+  };
+
+  // 선택된 상품들을 store 저장 후 주문 페이지로 route
+  const handlePlaceOrder = (selectedItems: CartListProps[] | undefined) => {
+    selectedItems
+      ? dispatch(setOrderProduct(selectedItems))
+      : dispatch(setOrderProduct(cartListArray));
+
+    dispatch(clearCartIndex());
+    // 주문 페이지 이동
+  };
+
   return (
     <Styled.CheckWrapper>
       <Styled.CartOrderBox>
         <Styled.OrderText>
-          총 <span>2</span>개의 상품금액
+          총 <span>{cartListArray?.length}</span>개의 상품금액
         </Styled.OrderText>
         <Styled.OrderText>
-          <span>25,800</span>원
+          <span>{handleTotalPrice()}</span>원
         </Styled.OrderText>
         <Plus />
         <Styled.OrderText>
@@ -22,13 +79,14 @@ const OrderCartList = () => {
         <Equals />
         <Styled.OrderText>합계</Styled.OrderText>
         <Styled.OrderText>
-          <span>25,800</span>원
+          <span>{handleTotalPrice()}</span>원
         </Styled.OrderText>
       </Styled.CartOrderBox>
 
       <Styled.ButtonWrapper>
         <div>
           <CartButtonComponent
+            onClick={handleRemoveCartList}
             backgroundColor={theme.colors.lightGray}
             padding="0.7"
             width="125"
@@ -41,6 +99,7 @@ const OrderCartList = () => {
 
         <div>
           <CartButtonComponent
+            onClick={() => handlePlaceOrder(getSelectedCartItems())}
             backgroundColor={theme.colors.cartButtonGray}
             padding="1"
             width="170"
@@ -50,6 +109,7 @@ const OrderCartList = () => {
             label="선택 상품 주문"
           />
           <CartButtonComponent
+            onClick={() => handlePlaceOrder(undefined)}
             backgroundColor={theme.colors.green3}
             padding="1"
             width="170"
