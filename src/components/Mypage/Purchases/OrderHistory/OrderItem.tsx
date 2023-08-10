b167de23bcbde4bfccba677bc91edbf8172af5b3
hyeone999 <caps026@naver.com>
2023-08-11 00:14:46 +0900

    feat: 주문 내역 조회시 받아오는 데이터 props 할당 및 처리

--- src/components/Mypage/Purchases/OrderHistory/OrderItem.tsx
+++ src/components/Mypage/Purchases/OrderHistory/OrderItem.tsx
@@ -1,14 +1,27 @@
-import theme from '@styles/theme';
 import React from 'react';
-import { OrderProps } from 'src/types/mypage/types';
+import theme from '@styles/theme';
 import { Styled } from '@components/Mypage/styles';
 import OrderInfoText from './OrderInfoText';
+import { OrderListProps } from 'src/types/mypage/types';
+import Image from 'next/image';
 
-export const OrderItem = ({ order }: { order: OrderProps }) => {
+export const OrderItem = ({ order }: { order: OrderListProps }) => {
   return (
     <Styled.FlexRowCenter>
-      <OrderInfoText size="20" color={theme.colors.black} text={order.date} />
-      <Styled.Image />
+      <OrderInfoText
+        size="20"
+        color={theme.colors.black}
+        text={order.serialNumber}
+      />
+      <Image
+        src={order.imgUrl}
+        priority
+        alt="식물 사진"
+        width="0"
+        height="0"
+        sizes="150px"
+        style={{ width: '150px', height: '150px' }}
+      />
       <Styled.ProductInfoWrapper>
         <OrderInfoText
           size="16"
@@ -16,9 +29,9 @@ export const OrderItem = ({ order }: { order: OrderProps }) => {
           text={order.productName}
         />
         <OrderInfoText
-          size="16"
+          size="13"
           color={theme.colors.mypageGray}
-          text={order.productDescription}
+          text={`[옵션] ${order.optionName}`}
         />
         {/* 상세보기 페이지로 이동 */}
         <Styled.ProductButton>상품 상세보기</Styled.ProductButton>
@@ -26,10 +39,18 @@ export const OrderItem = ({ order }: { order: OrderProps }) => {
       <OrderInfoText
         size="20"
         color={theme.colors.black}
-        text={order.quantity}
+        text={`${order.count}개`}
       />
-      <OrderInfoText size="20" color={theme.colors.black} text={order.price} />
-      <OrderInfoText size="20" color={theme.colors.black} text={order.status} />
+      <OrderInfoText
+        size="20"
+        color={theme.colors.black}
+        text={`${order.orderPrice.toLocaleString()}원`}
+      />
+      <OrderInfoText
+        size="20"
+        color={theme.colors.black}
+        text={order.orderStatus}
+      />
     </Styled.FlexRowCenter>
   );
 };
