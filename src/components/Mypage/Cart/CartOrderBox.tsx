9bebc7ce4673f67b0069d4c6419ccaba840d684c
hyeone999 <caps026@naver.com>
2023-08-11 00:12:44 +0900

    feat: OrderCartList 컴포넌트 구조적으로 분해 할당(컴포넌트화)

---
+++ src/components/Mypage/Cart/CartOrderBox.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Plus from '@assets/images/mypage/cartPlus.svg';
+import Equals from '@assets/images/mypage/cartEquals.svg';
+import { Styled } from '../styles';
+import { CartOrderBoxProps } from 'src/types/mypage/types';
+
+const CartOrderBox = ({
+  cartListArray,
+  handleTotalPrice,
+}: CartOrderBoxProps) => {
+  return (
+    <Styled.CartOrderBox>
+      <Styled.OrderText>
+        총 <span>{cartListArray?.length}</span>개의 상품금액
+      </Styled.OrderText>
+      <Styled.OrderText>
+        <span>{handleTotalPrice()}</span>원
+      </Styled.OrderText>
+      <Plus />
+      <Styled.OrderText>
+        배송비 <span>0</span>원
+      </Styled.OrderText>
+      <Equals />
+      <Styled.OrderText>합계</Styled.OrderText>
+      <Styled.OrderText>
+        <span>{handleTotalPrice()}</span>원
+      </Styled.OrderText>
+    </Styled.CartOrderBox>
+  );
+};
+
+export default CartOrderBox;
--- src/components/Mypage/Cart/OrderCartList.tsx
+++ src/components/Mypage/Cart/OrderCartList.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { Styled } from '../styles';
-import theme from '@styles/theme';
-import CartButtonComponent from './CartButtonComponent';
-import Plus from '@assets/images/mypage/cartPlus.svg';
-import Equals from '@assets/images/mypage/cartEquals.svg';
 import { CartListProps } from 'src/types/mypage/types';
 import { useSelector } from 'react-redux';
 import { clearCartIndex, setChecked } from 'store/reducers/cartSlice';
@@ -12,6 +8,8 @@ import { useDispatch } from 'react-redux';
 import { RootState } from 'store';
 import { getRemoveCartList } from 'src/apis/mypage/cart';
 import { setOrderProduct } from 'store/reducers/orderProductSlice';
+import CartOrderBox from './CartOrderBox';
+import CartButtonBox from './CartButtonBox';
 const cartSelector = (state: RootState) => state.cartIndex;
 
 const OrderCartList = ({
@@ -65,61 +63,16 @@ const OrderCartList = ({
 
   return (
     <Styled.CheckWrapper>
-      <Styled.CartOrderBox>
-        <Styled.OrderText>
-          총 <span>{cartListArray?.length}</span>개의 상품금액
-        </Styled.OrderText>
-        <Styled.OrderText>
-          <span>{handleTotalPrice()}</span>원
-        </Styled.OrderText>
-        <Plus />
-        <Styled.OrderText>
-          배송비 <span>0</span>원
-        </Styled.OrderText>
-        <Equals />
-        <Styled.OrderText>합계</Styled.OrderText>
-        <Styled.OrderText>
-          <span>{handleTotalPrice()}</span>원
-        </Styled.OrderText>
-      </Styled.CartOrderBox>
+      <CartOrderBox
+        cartListArray={cartListArray}
+        handleTotalPrice={handleTotalPrice}
+      />
 
-      <Styled.ButtonWrapper>
-        <div>
-          <CartButtonComponent
-            onClick={handleRemoveCartList}
-            backgroundColor={theme.colors.lightGray}
-            padding="0.7"
-            width="125"
-            size="14"
-            color={theme.colors.black}
-            weight="300"
-            label="선택 상품 삭제"
-          />
-        </div>
-
-        <div>
-          <CartButtonComponent
-            onClick={() => handlePlaceOrder(getSelectedCartItems())}
-            backgroundColor={theme.colors.cartButtonGray}
-            padding="1"
-            width="170"
-            size="15"
-            color={theme.colors.black}
-            weight="700"
-            label="선택 상품 주문"
-          />
-          <CartButtonComponent
-            onClick={() => handlePlaceOrder(undefined)}
-            backgroundColor={theme.colors.green3}
-            padding="1"
-            width="170"
-            size="15"
-            color={theme.colors.white}
-            weight="700"
-            label="전체 상품 주문"
-          />
-        </div>
-      </Styled.ButtonWrapper>
+      <CartButtonBox
+        getSelectedCartItems={getSelectedCartItems}
+        handlePlaceOrder={handlePlaceOrder}
+        handleRemoveCartList={handleRemoveCartList}
+      />
     </Styled.CheckWrapper>
   );
 };
