aca1650bc03f34bc42d520cb4e65577ca61b3701
blue7918 <kkiu978@gmail.com>
2023-08-11 09:41:11 +0900

    fix: category router, useEffect

--- src/components/Shop/Common/Category/index.tsx
+++ src/components/Shop/Common/Category/index.tsx
@@ -1,42 +1,37 @@
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
+import Styled from './styles';
+import { setCategoryId } from 'store/reducers/categorySlice';
 import { getProductCategory } from 'src/apis/common/category';
 import { ICategory } from 'src/types/home/types';
-import { setCategoryId } from 'store/reducers/categorySlice';
-import Styled from './styles';
 import Plant from './Plant';
+
 
 const Category = () => {
   const dispatch = useDispatch();
-  const router = useRouter();
-  const menu = router.query.category;
-  let category: string;
-  if (menu) {
-    category = menu.toString();
-  }
-  
   const [categoryList, setCategoryList] = useState([]);
   const handleCategoryList = async () => {
     const response = await getProductCategory();
     setCategoryList(response);
   };
+  const handleDispatch = (id: string) => {
+    dispatch(setCategoryId(id));
+  }
   useEffect(() => {
     handleCategoryList();
   }, []);
-  useEffect(() => {
-    dispatch(setCategoryId(category))
-  }, [category])
   return (
     <Styled.Wrapper>
       <Styled.Title>카테고리</Styled.Title>
       <Styled.Plants>
         {categoryList &&
           categoryList?.map((plant: ICategory) => (
-            <Link href={`/shop/${plant.categoryName}`} key={plant.categoryId}>
-              <Plant name={plant.categoryName} src={plant.imgUrl} />
-            </Link>
+            <div key={plant.categoryId} onClick={()=>handleDispatch(plant.categoryName)}>
+              <Link href={`/shop/${plant.categoryName}`} >
+                <Plant name={plant.categoryName} src={plant.imgUrl} />
+              </Link>
+            </div>
           ))}
       </Styled.Plants>
     </Styled.Wrapper>
