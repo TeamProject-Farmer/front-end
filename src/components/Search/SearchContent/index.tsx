1024ff86853532c75b06027362564bc5bdd2e0ae
zwonkim <k980912@naver.com>
2023-08-13 15:20:34 +0900

    feat: 검색결과가 없는 경우 조건 처리

--- src/components/Search/SearchContent/index.tsx
+++ src/components/Search/SearchContent/index.tsx
@@ -5,24 +5,28 @@ import { ProductProps } from '../../Common/type';
 
 const SearchContent = ({ searchedWord, searchResult }) => {
   return (
-    <Styled.Wrapper>
+    <>
       {searchResult && searchResult.length === 0 && (
-        <div>'{searchedWord}'에 대한 검색결과가 존재하지 않습니다</div>
+        <Styled.NoResult>
+          <span>'{searchedWord}'</span>에 대한 검색결과가 존재하지 않습니다
+        </Styled.NoResult>
       )}
-      {/* 검색결과가 존재하지 않을 때, 로딩 추가 */}
-      {searchResult &&
-        searchResult?.map((plant: ProductProps, index: number) => (
-          <Plant
-            key={index}
-            thumbnailImg={plant.thumbnailImg}
-            name={plant.name}
-            discountRate={plant.discountRate}
-            price={plant.price}
-            averageStarRating={plant.averageStarRating}
-            reviewCount={plant.reviewCount}
-          />
-        ))}
-    </Styled.Wrapper>
+      <Styled.Wrapper>
+        {/* 로딩 추가 */}
+        {searchResult &&
+          searchResult?.map((plant: ProductProps, index: number) => (
+            <Plant
+              key={index}
+              thumbnailImg={plant.thumbnailImg}
+              name={plant.name}
+              discountRate={plant.discountRate}
+              price={plant.price}
+              averageStarRating={plant.averageStarRating}
+              reviewCount={plant.reviewCount}
+            />
+          ))}
+      </Styled.Wrapper>
+    </>
   );
 };
 
@@ -31,8 +35,15 @@ export default SearchContent;
 const Styled = {
   Wrapper: styled.div`
     display: grid;
+    place-items: center;
     grid-template-columns: repeat(4, 1fr);
     gap: 37px;
     padding-bottom: 130px;
+  `,
+  NoResult: styled.div`
+    font-size: 20px;
+    span {
+      font-size: 22px;
+    }
   `,
 };
