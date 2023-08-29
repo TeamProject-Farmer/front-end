7f095cde0522e6c32d92cffc36272b9088c0e269
blue7918 <kkiu978@gmail.com>
2023-08-29 17:39:38 +0900

    design: 샵 index 페이지 디자인 변경 적용 event 페이지 이동이 애매해져 디자이너분께 문의 드려놓은 상태이며 추후 그 부분 수정 예정

--- src/components/Shop/index.tsx
+++ src/components/Shop/index.tsx
@@ -1,22 +1,22 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import styled from '@emotion/styled';
 import theme from '@styles/theme';
+import Category from '@components/Common/Category';
 
 const Shop = () => {
-  const Add1: string = '식물 키우기 어려우시죠?';
-  const Add2: string = '내 반려식물을 등록하고 관리해보세요.';
+  const event1: string = '/assets/images/shop/eventBanner1.svg';
+  const event2: string = '/assets/images/shop/eventBanner2.svg';
   return (
     <Styled.Wrapper>
+      <Category />
       <Styled.EventWrapper>
+        <Styled.SmallEvent>
+          <Image src={event1} alt="event1" width={1194} height={226} />
+        </Styled.SmallEvent>
         <Link href="/shop/bysize/s">
           <Styled.NormalEvent>사이즈별</Styled.NormalEvent>
         </Link>
-        <Styled.SmallEvent>
-          <div>
-            <span>{Add1}</span>
-            <span>{Add2}</span>
-          </div>
-        </Styled.SmallEvent>
         <Link href="/shop/event/1">
           <Styled.NormalEvent>이벤트 페이지</Styled.NormalEvent>
         </Link>
@@ -30,45 +30,34 @@ const Styled = {
     width: 100vw;
     max-width: 100%;
     min-width: ${theme.size.shopDetailMinWidth};
-    height: 100vh;
-    max-height: 100%;
+    height: fit-content;
   `,
   EventWrapper: styled.div`
     width: 100%;
-    height: 1351px;
-    padding: 45px 363px;
+    height: 1189px;
+    padding: 45px 0;
     display: flex;
     flex-direction: column;
+    align-items: center;
     border-top: 1px solid ${theme.colors.black};
+    background-color: ${theme.colors.white};
   `,
   SmallEvent: styled.div`
     height: 226px;
-    width: 100%;
+    width: 1194px;
     display: flex;
     align-items: center;
     justify-content: center;
     border-radius: 20px;
     background: #d9d9d9;
     margin: 25px 0;
-    & > div {
-      display: flex;
-      flex-direction: column;
-      text-align: left;
-      & > span {
-        color: #000;
-        font-size: 25px;
-        font-weight: 500;
-      }
-      & > span:last-child {
-        font-weight: 700;
-      }
-    }
+    overflow: hidden;
   `,
   NormalEvent: styled.div`
     height: 330px;
     font-size: 64px;
     font-weight: 700;
-    width: 100%;
+    width: 1194px;
     display: flex;
     align-items: center;
     justify-content: center;
