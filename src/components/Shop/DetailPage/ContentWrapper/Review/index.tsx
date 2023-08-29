19e07e240bf113e78a02156427b1dc4f475940a7
blue7918 <kkiu978@gmail.com>
2023-08-29 17:38:15 +0900

    fix: review reset 시간 줄임

--- src/components/Shop/DetailPage/ContentWrapper/Review/index.tsx
+++ src/components/Shop/DetailPage/ContentWrapper/Review/index.tsx
@@ -70,7 +70,7 @@ const Review = () => {
     handleReviewData();
   }, [router, currentIndex, sortOption, starOption]);
   useEffect(() => {
-    setTimeout(() => handleReviewData(), 30);
+    setTimeout(() => handleReviewData(), 10);
   }, [reviewClick]);
   useEffect(() => {
     setErrorMessage(false);
