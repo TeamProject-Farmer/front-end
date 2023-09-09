6dc9d40a0761455c779df239a5b993b34ebe89d3
blue7918 <kkiu978@gmail.com>
2023-09-09 21:55:52 +0900

    feat: shop index pages 하나로 통일

--- src/components/Shop/styles.ts
+++ src/components/Shop/styles.ts
@@ -114,3 +114,45 @@ export const EventStyled = {
     margin-bottom: 24px;
   `,
 };
+
+export const ShopIndexStyled = {
+  Wrapper: styled.div`
+    width: 100vw;
+    max-width: 100%;
+    min-width: ${theme.size.shopDetailMinWidth};
+    height: fit-content;
+  `,
+  EventWrapper: styled.div`
+    width: 100%;
+    height: 1189px;
+    padding: 45px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-top: 1px solid ${theme.colors.black};
+    background-color: ${theme.colors.white};
+  `,
+  SmallEvent: styled.div`
+    height: 226px;
+    width: 1194px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 20px;
+    background: #d9d9d9;
+    margin: 25px 0;
+    overflow: hidden;
+  `,
+  NormalEvent: styled.div`
+    height: 330px;
+    font-size: 64px;
+    font-weight: 700;
+    width: 1194px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 20px;
+    background: #d9d9d9;
+    margin: 25px 0;
+  `,
+};
