49424ef2e80cd9e4912ecf0bfc5b8de4a1cca3f8
blue7918 <kkiu978@gmail.com>
2023-09-09 18:07:55 +0900

    feat: pages coupon으로 shop coupon 하나로 합침

--- src/components/Shop/styles.ts
+++ src/components/Shop/styles.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import theme from '@styles/theme';
+import arrow79 from '@assets/images/shop/arrow79.svg';
 
 export const BySizeStyled = {
   Wrapper: styled.div`
@@ -16,4 +17,49 @@ export const BySizeStyled = {
     justify-content: center;
     height: fit-content;
   `,
+};
+
+export const FirstCouponStyled = {
+  Wrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    min-width: ${theme.size.shopDetailMinWidth};
+    width: 100vw;
+    max-width: 99.7%;
+  `,
+  ContentWrapper: styled.div`
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 3350px;
+    padding: 110px;
+    border-top: 2px solid #585858;
+  `,
+  ImageBox: styled.div`
+    position: relative;
+  `,
+  Button: styled.button`
+    position: absolute;
+    top: 1262px;
+    left: 255px;
+    border: none;
+    border-radius: 10px;
+    width: 490px;
+    height: 130px;
+    background-color: ${theme.colors.green1};
+    color: #fff;
+    font-size: 45px;
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 18px;
+  `,
+  Arrow: styled(arrow79)``,
 };
--- src/pages/shop/coupon/index.tsx
+++ src/pages/shop/coupon/index.tsx
@@ -1,11 +1,34 @@
 import { ReactElement } from 'react';
+import Image from 'next/image';
 import type { NextPageWithLayout } from "@pages/_app";
 import Layout from "@pages/layout";
-import FirstCoupon from '@components/Shop/FirstCoupon';
+import Category from '@components/Common/Category';
+import SideAd from '@components/Shop/Common/SideAd';
+import { FirstCouponStyled as Styled } from '@components/Shop/styles';
 
 const BySizePage: NextPageWithLayout = () => {
-  return <FirstCoupon />;
+  return (
+    <Styled.Wrapper>
+      <Category />
+      <Styled.ContentWrapper>
+        <SideAd top={170} left={400} />
+        <Styled.ImageBox>
+          <Image
+            src="/assets/images/shop/firstCouponImg.svg"
+            alt="download-coupon"
+            width={1000}
+            height={3097}
+          />
+          <Styled.Button onClick={()=>alert('신규 가입 쿠폰이 발급되었습니다.')}>
+            다운받기
+            <Styled.Arrow />
+          </Styled.Button>
+        </Styled.ImageBox>
+      </Styled.ContentWrapper>
+    </Styled.Wrapper>
+  );
 }
+
 BySizePage.getLayout = function getLayout(page: ReactElement) {
   return <Layout>{page}</Layout>;
 };
