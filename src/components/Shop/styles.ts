46b50eb2da5d1c71465c06f7a1cdc156a2c1f4de
blue7918 <kkiu978@gmail.com>
2023-09-09 22:31:20 +0900

    feat: shop category에 따른 detail pages 통일

--- src/components/Shop/styles.ts
+++ src/components/Shop/styles.ts
@@ -157,6 +157,34 @@ export const ShopIndexStyled = {
   `,
 };
 
+export const ShopPageStyled = {
+  Wrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: ${theme.colors.black};
+    text-align: center;
+    min-width: ${theme.size.shopDetailMinWidth};
+  `,
+  Title: styled.div`
+    height: 130px;
+    width: 100%;
+    font-size: 30px;
+    font-weight: 700;
+    background-color: #ecf9e9;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  ContentWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 110px;
+    align-items: center;
+  `,
+};
+
 export const DetailPageStyled = {
   Wrapper: styled.div`
     display: flex;
--- src/utils/register/scrollUp.ts
+++ src/utils/register/scrollUp.ts
@@ -1,4 +1,4 @@
-// 스크롤 제일 위로 (추후 다른 페이지에도 쓰일수 있을거 같아서 일단 util에 생성)
-export const scrollToTop = () => {
-  window.scrollTo(0, 0);
+// 스크롤 제일 위로
+export const scrollToTop = (height?: number) => {
+  height? window.scrollTo(0, height) : window.scrollTo(0, 0);
 };
