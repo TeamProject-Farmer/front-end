30fe84ba386ae93172226558bbe0b21cf7034139
zwonkim <k980912@naver.com>
2023-09-11 15:46:43 +0900

    feat: redirect url 변경

--- src/utils/login/socialLoginData.ts
+++ src/utils/login/socialLoginData.ts
@@ -2,8 +2,8 @@ import Google from '@assets/images/login/google.svg';
 import Kakao from '@assets/images/login/kakao.svg';
 import Naver from '@assets/images/login/naver.svg';
 
-const address = 'http://localhost:3000';
-// const address = 'https://front-end-farmer-shop.vercel.app'
+// const address = 'http://localhost:3000';
+const address = 'https://front-end-farmer-shop.vercel.app';
 
 // 카카오
 const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
