72180c3bbeac81a3f1e244454ecc77065cd3a07a
blue7918 <kkiu978@gmail.com>
2023-09-11 11:31:01 +0900

    refactor: staroption map 추가

--- src/components/Shop/DetailPage/ContentWrapper/Review/StarOption.tsx
+++ src/components/Shop/DetailPage/ContentWrapper/Review/StarOption.tsx
@@ -3,66 +3,39 @@ import theme from '@styles/theme';
 import { StarOptionProps } from 'src/types/shop/types';
 import downArrow from '@assets/images/shop/downArrow1.svg';
 
-const StarOption = (props : StarOptionProps) => {
-  const {setPopStarOption, popStarOption, setStarOption, starOption } = props;
+const StarOption = (props: StarOptionProps) => {
+  const { setPopStarOption, popStarOption, setStarOption, starOption } = props;
+  const starArray: number[] = [5, 4, 3, 2, 1];
 
-  return (<Styled.StarOptionWrapper
-    onClick={() => setPopStarOption(!popStarOption)}
-  >
-    {starOption == null ? '전체' : starOption+'점'}
-    <Styled.DownArrow />
-    <Styled.StarOptions popStarOption={popStarOption}>
-      <Styled.EachStarOption
-        onClick={() => {
-          setStarOption(null);
-          setPopStarOption(false);
-        }}
-      >
-        전체
-      </Styled.EachStarOption>
-      <Styled.EachStarOption
-        onClick={() => {
-          setStarOption(5);
-          setPopStarOption(false);
-        }}
-      >
-        5점
-      </Styled.EachStarOption>
-      <Styled.EachStarOption
-        onClick={() => {
-          setStarOption(4);
-          setPopStarOption(false);
-        }}
-      >
-        4점
-      </Styled.EachStarOption>
-      <Styled.EachStarOption
-        onClick={() => {
-          setStarOption(3);
-          setPopStarOption(false);
-        }}
-      >
-        3점
-      </Styled.EachStarOption>
-      <Styled.EachStarOption
-        onClick={() => {
-          setStarOption(2);
-          setPopStarOption(false);
-        }}
-      >
-        2점
-      </Styled.EachStarOption>
-      <Styled.EachStarOption
-        onClick={() => {
-          setStarOption(1);
-          setPopStarOption(false);
-        }}
-      >
-        1점
-      </Styled.EachStarOption>
-    </Styled.StarOptions>
-  </Styled.StarOptionWrapper>);
-}
+  return (
+    <Styled.StarOptionWrapper onClick={() => setPopStarOption(!popStarOption)}>
+      {starOption == null ? '전체' : starOption + '점'}
+      <Styled.DownArrow />
+      <Styled.StarOptions popStarOption={popStarOption}>
+        <Styled.EachStarOption
+          onClick={() => {
+            setStarOption(null);
+            setPopStarOption(false);
+          }}
+        >
+          전체
+        </Styled.EachStarOption>
+        <>
+          {starArray.map((star: number) => (
+            <Styled.EachStarOption
+              onClick={() => {
+                setStarOption(star);
+                setPopStarOption(false);
+              }}
+            >
+              {star}점
+            </Styled.EachStarOption>
+          ))}
+        </>
+      </Styled.StarOptions>
+    </Styled.StarOptionWrapper>
+  );
+};
 
 const Styled = {
   StarOptionWrapper: styled.div`
@@ -99,7 +72,7 @@ const Styled = {
       background-color: #ecf9e9;
     }
   `,
-    DownArrow: styled(downArrow)``,
-}
+  DownArrow: styled(downArrow)``,
+};
 
 export default StarOption;
