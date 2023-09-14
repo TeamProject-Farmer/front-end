ee78c6c11ccd23e18a89415d05a462091c7dc9f1
blue7918 <kkiu978@gmail.com>
2023-09-14 13:37:29 +0900

    fix: panel 호출 잘 안되던 if 문 삭제

--- src/components/Shop/DetailPage/Panel/index.tsx
+++ src/components/Shop/DetailPage/Panel/index.tsx
@@ -20,47 +20,46 @@ const Panel = (props: PanelProps) => {
 
   const router = useRouter();
   const productId = Number(router.query?.detail);
-  const { totalStar, starRating} = usePanelStars(productId);
+  const { totalStar, starRating } = usePanelStars(productId);
 
-  if (totalStar && starRating) {
-    return (
-      <Styled.Wrapper>
-        <Styled.InnerBox>
-          <Styled.ImageBox>
-            <Image
-              src={productData.thumbnailImg}
-              alt="Thumbnail-Imgage"
-              className="imageStyle"
-              width={548.55}
-              height={547.55}
-              priority={true}
-            ></Image>
-          </Styled.ImageBox>
-          <Styled.ContentWrapper>
-            <ProductInfo
-              productId={productId}
-              name={productData.name}
-              totalStar={totalStar}
-              starRating={starRating}
-              discountRate={productData.discountRate}
-              price={productData.price}
-            />
-            <PayInfo />
-            <Styled.VerticalLine />
-            <OptionBox
-              isPanel={true}
-              optionList={optionList}
-              setSelectList={setSelectList}
-              selectList={selectList}
-              setSelectPrice={setSelectPrice}
-              selectPrice={selectPrice}
-            />
-          </Styled.ContentWrapper>
-        </Styled.InnerBox>
-      </Styled.Wrapper>
-    );
-  }
+  return (
+    <Styled.Wrapper>
+      <Styled.InnerBox>
+        <Styled.ImageBox>
+          <Image
+            src={productData.thumbnailImg}
+            alt="Thumbnail-Imgage"
+            className="imageStyle"
+            width={548.55}
+            height={547.55}
+            priority={true}
+          ></Image>
+        </Styled.ImageBox>
+        <Styled.ContentWrapper>
+          <ProductInfo
+            productId={productId}
+            name={productData.name}
+            totalStar={totalStar}
+            starRating={starRating}
+            discountRate={productData.discountRate}
+            price={productData.price}
+          />
+          <PayInfo />
+          <Styled.VerticalLine />
+          <OptionBox
+            isPanel={true}
+            optionList={optionList}
+            setSelectList={setSelectList}
+            selectList={selectList}
+            setSelectPrice={setSelectPrice}
+            selectPrice={selectPrice}
+          />
+        </Styled.ContentWrapper>
+      </Styled.InnerBox>
+    </Styled.Wrapper>
+  );
 };
+
 const Styled = {
   Wrapper: styled.div`
     display: flex;
