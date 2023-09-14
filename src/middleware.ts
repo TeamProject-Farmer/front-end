36d97f113e693be416c66dc36c2c4f6800bf77e5
zwonkim <k980912@naver.com>
2023-09-14 15:19:07 +0900

    feat: 로그인 유무에 따른 미들웨어 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,23 @@ export function middleware(request: NextRequest, event: NextFetchEvent) {
   const refreshToken = request.cookies.get('refreshToken')?.value;
   const pathname = request.nextUrl.pathname;
 
-  // if (refreshToken) {
-  //   if (pathname.startsWith('/login')) {
-  //     return NextResponse.redirect(
-  //       new URL('/?alert=이미 로그인한 사용자입니다.', request.url),
-  //     );
-  //   }
-  //   if (pathname.startsWith('/register')) {
-  //     return NextResponse.redirect(
-  //       new URL('/?alert=이미 가입된 회원입니다.', request.url),
-  //     );
-  //   }
-  // }
+  if (refreshToken) {
+    console.log('pathname', pathname);
+    if (pathname === '/login') {
+      return NextResponse.redirect(
+        new URL('/?alert=이미 로그인한 사용자입니다.', request.url),
+      );
+    }
+    if (pathname === '/register') {
+      return NextResponse.redirect(
+        new URL('/?alert=이미 가입된 회원입니다.', request.url),
+      );
+    }
+  }
+
+  if (!refreshToken && pathname.startsWith('/mypage')) {
+    return NextResponse.redirect(
+      new URL('/?alert=로그인 후 이용 가능한 서비스입니다.', request.url),
+    );
+  }
 }
