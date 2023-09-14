7427b3e5a383c5094fb13e18ed24b598e51b1e82
blue7918 <kkiu978@gmail.com>
2023-09-14 13:49:45 +0900

    style: review 하나의 style 파일로

--- src/components/Shop/DetailPage/ContentWrapper/Review/StarOption.tsx
+++ src/components/Shop/DetailPage/ContentWrapper/Review/StarOption.tsx
@@ -1,7 +1,6 @@
-import styled from '@emotion/styled';
-import theme from '@styles/theme';
 import { StarOptionProps } from 'src/types/shop/types';
-import downArrow from '@assets/images/shop/downArrow1.svg';
+import { StarOptionStyled as Styled } from './styles';
+
 
 const StarOption = (props: StarOptionProps) => {
   const { setPopStarOption, popStarOption, setStarOption, starOption } = props;
@@ -37,42 +36,6 @@ const StarOption = (props: StarOptionProps) => {
   );
 };
 
-const Styled = {
-  StarOptionWrapper: styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    width: 63px;
-    height: 34px;
-    border-radius: 5px;
-    background-color: #ecf9e9;
-    color: ${theme.colors.green1};
-    position: relative;
-  `,
-  StarOptions: styled.div<{ popStarOption: boolean }>`
-    height: fit-content;
-    width: 63px;
-    border-radius: 5px;
-    background-color: #fafbf9;
-    color: ${theme.colors.green1};
-    border: 1px solid #ccdcc9;
-    position: absolute;
-    top: 40px;
-    left: 0;
-    display: ${props => (props.popStarOption ? 'flex' : 'none')};
-    flex-direction: column;
-  `,
-  EachStarOption: styled.div`
-    width: 100%;
-    height: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    &:hover {
-      background-color: #ecf9e9;
-    }
-  `,
-  DownArrow: styled(downArrow)``,
-};
+
 
 export default StarOption;
--- src/components/Shop/DetailPage/ContentWrapper/Review/index.tsx
+++ src/components/Shop/DetailPage/ContentWrapper/Review/index.tsx
@@ -2,8 +2,6 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useQuery } from '@tanstack/react-query';
 import { setTimeout } from 'timers';
-import styled from '@emotion/styled';
-import theme from '@styles/theme';
 import { getReview, getReviewStar } from 'src/apis/shop/review';
 import { SingleReviewProps } from 'src/types/shop/types';
 import VerticalLine from '@components/Shop/Common/SmallParts/VerticalLine';
@@ -12,8 +10,7 @@ import EachStarGauge from '@components/Shop/Common/GaugeBox/EachStarGauge';
 import SingleReview from './SingleReview';
 import StarOption from './StarOption';
 import Pagination from '@components/Shop/Common/Pagination';
-import photo from '@assets/images/shop/photoIcon.svg';
-import downArrow from '@assets/images/shop/downArrow1.svg';
+import { ReviewStyled as Styled } from './styles';
 
 const Review = () => {
   const router = useRouter();
@@ -147,123 +144,6 @@ const Review = () => {
   }
 };
 
-const Styled = {
-  Wrapper: styled.div`
-    width: ${theme.size.shopDetailWidth};
-    margin-bottom: 86px;
-  `,
-  Title: styled.div`
-    color: #000;
-    font-size: 20px;
-    font-weight: 700;
-    display: flex;
-    & > div:last-child {
-      color: #47ac3a;
-      margin-left: 6px;
-    }
-  `,
-  OptionBox: styled.div`
-    height: 39px;
-    border-radius: 5px;
-    background-color: #ecf9e9;
-    padding: 0 16px;
-    margin: 20px 0;
-    display: flex;
-    align-items: center;
-    position: relative;
-    & > svg {
-      position: absolute;
-      right: 17px;
-    }
-  `,
-  DownArrow: styled(downArrow)``,
-  TotalLike: styled.div`
-    border-radius: 5px;
-    border: 1px solid #000;
-    height: 148px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-left: 54px;
-    padding-right: 71px;
-    margin-bottom: 27px;
-    & > div:first-child {
-      color: #424242;
-      text-align: center;
-      font-size: 30px;
-      font-weight: 700;
-      display: flex;
-      align-items: center;
-    }
-    & > div:first-child > div:last-child {
-      margin-left: 17.5px;
-    }
-    & > :nth-child(2) {
-      width: 1px;
-      height: 100.5px;
-      background-color: #747474;
-    }
-    & > div:last-child {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-    }
-    & > div:last-child > div {
-      width: 240px;
-    }
-  `,
-  ReviewTitle: styled.div`
-    height: 58px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border-top: 1px solid ${theme.colors.green1};
-    border-bottom: 1px solid ${theme.colors.green1};
-    font-size: 14px;
-    font-weight: 700;
-    cursor: pointer;
-    & > div {
-      display: flex;
-    }
-    & > div > div {
-      margin-right: 11px;
-    }
-  `,
-  BestSort: styled.div<{ sortOption: string }>`
-    color: ${props => (props.sortOption == 'best' ? '#59B941' : '')};
-  `,
-  RecentSort: styled.div<{ sortOption: string }>`
-    color: ${props => (props.sortOption == 'recent' ? '#59B941' : '')};
-  `,
 
-  PhotoReviewBtn: styled.div`
-    display: flex;
-    & > div {
-      margin-left: 5px;
-    }
-  `,
-  PhotoIcon: styled(photo)``,
-  NoData: styled.div`
-    text-align: center;
-    font-size: 20px;
-    font-weight: 500;
-    color: ${theme.colors.lightGray};
-  `,
-  ErrorMessage: styled.div`
-    width: 100%;
-    height: 450px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding-top: 100px;
-    font-size: 25px;
-    font-weight: 600;
-    color: ${theme.colors.lightGray};
-  `,
-  PaginationBox: styled.div`
-    margin-top: 33px;
-  `,
-};
 
 export default Review;
