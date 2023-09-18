718a29b391f4d43d5b985cd364ba2ef5911f8cf5
blue7918 <kkiu978@gmail.com>
2023-09-18 19:27:51 +0900

    feat: board manage page

--- src/components/Admin/styles.ts
+++ src/components/Admin/styles.ts
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import theme from '@styles/theme';
+import rightSmallArrow from '@assets/images/admin/rightSmallArrow.svg';
 
 export const HomeStyled = {
   Wrapper: styled.div`
@@ -9,5 +11,130 @@ export const HomeStyled = {
     padding-top: 8px;
     padding-bottom: 20px;
     cursor: pointer;
+  `,
+};
+
+export const BoardManageStyled = {
+  WholeWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  `,
+  Wrapper: styled.div`
+    width: 1616px;
+    min-height: 632px;
+    border-radius: 5px;
+    background: #fff;
+  `,
+  BackButton: styled.div`
+    color: ${theme.adminColors.black};
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 140%; /* 19.6px */
+    letter-spacing: -0.056px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    cursor: pointer;
+  `,
+  BackIcon: styled(rightSmallArrow)``,
+  Footer: styled.div`
+    border-top: 1px solid #e5e5e5;
+    padding-top: 16px;
+    display: flex;
+    width: 100%;
+    height: 178px;
+  `,
+  ContentWrapper: styled.div`
+    width: 100%;
+    min-height: 453px;
+    padding: 32px 36px 32px 36px;
+    display: flex;
+    flex-direction: column;
+  `,
+  Title: styled.div`
+    color: #8a8a8a;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 140%; /* 16.8px */
+    letter-spacing: -0.048px;
+  `,
+  ContentTitle: styled.div`
+    color: ${theme.adminColors.black};
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 140%; /* 28px */
+    letter-spacing: -0.08px;
+  `,
+  ProfileWrapper: styled.div`
+    display: flex;
+    gap: 16px;
+    margin-top: 8px;
+    margin-bottom: 20px;
+  `,
+  ProfileImage: styled.div`
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+    background-color: #b9b9b9;
+  `,
+  ProfileBox: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 140%; /* 16.8px */
+    letter-spacing: -0.048px;
+    & div {
+      display: flex;
+      gap: 7px;
+    }
+    & div > div:first-of-type {
+      color: #b9b9b9;
+    }
+    & div > div:last-of-type {
+      color: ${theme.adminColors.black};
+    }
+  `,
+  ContentBox: styled.div`
+    color: ${theme.adminColors.black};
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 140%; /* 16.8px */
+    letter-spacing: -0.048px;
+  `,
+  FooterWriter: styled.div`
+    color: #1a1c1e;
+    text-align: right;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 140%;
+    letter-spacing: -0.048px;
+    margin-top: 5px;
+    margin-left: 60px;
+  `,
+  InputBox: styled.textarea`
+    margin-left: 29px;
+    margin-right: 14px;
+    width: 1326px;
+    height: 144px;
+    border-radius: 4px;
+    border: 1px solid #e5e5e5;
+    resize: none;
+    padding: 20px;
+  `,
+  SaveButton: styled.button`
+    border-radius: 3px;
+    background: #5d5d5d;
+    width: 104px;
+    height: 41px;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    font-size: 16px;
+    font-weight: 500;
+    letter-spacing: -0.064px;
   `,
 };
--- src/pages/admin/board/manage.tsx
+++ src/pages/admin/board/manage.tsx
@@ -1,9 +1,50 @@
 import { ReactElement } from 'react';
+import { useRouter } from 'next/router';
 import type { NextPageWithLayout } from '@pages/_app';
 import AdminLayout from '@pages/adminlayout';
+import { BoardManageStyled as Styled } from '@components/Admin/styles';
 
 const BoardManagePage: NextPageWithLayout = () => {
-  return <div></div>;
+  const router = useRouter();
+  return (
+    <Styled.WholeWrapper>
+      <Styled.BackButton onClick={()=>router.push('/admin/board')}>
+        <Styled.BackIcon />
+        게시글 리스트로 돌아가기
+      </Styled.BackButton>
+      <Styled.Wrapper>
+        <Styled.ContentWrapper>
+          <Styled.Title>Q&A</Styled.Title>
+          {/* 현재 들어있는 데이터들은 임시 데이터이며*/}
+          {/* 추후 데이터를 받아와 넣어주는 방식으로 변경될 예정입니다. */}
+          <Styled.ContentTitle>문의합니다</Styled.ContentTitle>
+          <Styled.ProfileWrapper>
+            <Styled.ProfileImage></Styled.ProfileImage>
+            <Styled.ProfileBox>
+              <div>
+                <div>작성자</div>
+                <div>김파머</div>
+              </div>
+              <div>
+                <div>작성일</div>
+                <div>2023.07.05</div>
+              </div>
+              <div>
+                <div>조회수</div>
+                <div>2</div>
+              </div>
+            </Styled.ProfileBox>
+          </Styled.ProfileWrapper>
+          <Styled.ContentBox>제품에 대해 문의합니다.</Styled.ContentBox>
+        </Styled.ContentWrapper>
+        <Styled.Footer>
+          <Styled.FooterWriter>관리자</Styled.FooterWriter>
+          <Styled.InputBox></Styled.InputBox>
+          <Styled.SaveButton>저장</Styled.SaveButton>
+        </Styled.Footer>
+      </Styled.Wrapper>
+    </Styled.WholeWrapper>
+  );
 };
 
 BoardManagePage.getLayout = function getLayout(page: ReactElement) {
