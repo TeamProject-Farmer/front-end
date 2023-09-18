2f6335040a9449468ef932f0b484d41336332a22
blue7918 <kkiu978@gmail.com>
2023-09-18 17:45:30 +0900

    style: layout 수정

---
+++ src/components/Admin/styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const HomeStyled = {
+  Wrapper: styled.div`
+    width: 100%;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    padding-top: 8px;
+    padding-bottom: 20px;
+    cursor: pointer;
+  `,
+};
