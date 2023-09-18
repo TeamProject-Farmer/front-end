12f2ba6b7d5ba923e74f4692c6c5555cbaa5f683
blue7918 <kkiu978@gmail.com>
2023-09-18 17:45:03 +0900

    feat: pages home으로 통일

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,10 +1,23 @@
 import { ReactElement } from 'react';
-import type { NextPageWithLayout } from "@pages/_app";
+import type { NextPageWithLayout } from '@pages/_app';
 import AdminLayout from '@pages/adminlayout';
-import Home from '@components/Admin/Home';
+import Analytics from '@components/Admin/Home/Analytics';
+import Board from '@components/Admin/Home/Board';
+import Todo from '@components/Admin/Home/Todo';
+import Title from '@components/Home/Common/Title';
+import { HomeStyled as Styled } from '@components/Admin/styles';
 
 const AdminPage: NextPageWithLayout = () => {
-  return(<Home />)
+  return (
+    <Styled.Wrapper>
+      <Title title="오늘의 할 일" />
+      <Todo />
+      <Title title="통계" />
+      <Analytics />
+      <Title title="게시판" />
+      <Board />
+    </Styled.Wrapper>
+  );
 };
 
 AdminPage.getLayout = function getLayout(page: ReactElement) {
@@ -12,4 +25,3 @@ AdminPage.getLayout = function getLayout(page: ReactElement) {
 };
 
 export default AdminPage;
-
